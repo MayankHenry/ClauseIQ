b72092ff5bc55ed75a7a81b682881d892aac3ff8
Henry <henry@example.com>
2026-09-24 20:23:05 +0530

    Fix Alembic database configuration

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -2,7 +2,6 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 import sys
@@ -13,6 +12,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from app.models.db import Base  # noqa
 
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
